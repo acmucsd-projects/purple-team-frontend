3bd3752cd0a414ea8496163116dd8e48462fc3d3
michl1001 <mlshaojr@gmail.com>
2020-05-02 15:08:01 -0400

    Basic Navbar setup

---
+++ src/general_components/navBar/indext.jsx
@@ -0,0 +1,13 @@
+import './style.less';
+class NavBar extends React.Component {
+    render() {
+      return (
+      
+       <div className=''>
+           
+       </div>
+      
+      );
+
+    }
+  }
