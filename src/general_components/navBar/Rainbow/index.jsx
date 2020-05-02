793a0f4eb72ca887f03f319a7f1b77ef15fad978
Ishaan Harry Kavoori <ishaanharry@live.com>
2020-05-02 12:45:22 -0700

    created Rainbow class in NavBar

---
+++ src/general_components/navBar/Rainbow/index.jsx
@@ -0,0 +1,12 @@
+import './style.less';
+class Rainbow extends React.Component {
+    render() {
+      return (
+        <span class="pinkFill"></span>
+        <span class="greenFill"></span>
+        <span class="cyanFill"></span>
+        <span class="purpleFill"></span>
+        <span class="orangeFill"></span>
+      );
+    }
+}
