6f8f722bcdc727dbfd0ff2370d207c645b8306ed
michl1001 <mlshaojr@gmail.com>
2020-05-03 16:16:24 -0400

    NavBar displays on default page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page from './pages';
 
 function App() {
   return (
+  
     <div className="App">
+      <Page />
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/general_components/navBar/Rainbow/index.jsx
+++ src/general_components/navBar/Rainbow/index.jsx
@@ -1,12 +1,16 @@
 import './style.less';
 class Rainbow extends React.Component {
     render() {
-      return (
-        <span class="pinkFill"></span>
-        <span class="greenFill"></span>
-        <span class="cyanFill"></span>
-        <span class="purpleFill"></span>
-        <span class="orangeFill"></span>
-      );
+      
+      return(
+        <div>
+          <span class="pinkFill"></span>
+          <span class="greenFill"></span>
+          <span class="cyanFill"></span>
+          <span class="purpleFill"></span>
+          <span class="orangeFill"></span>
+        </div>
+        );
+    
     }
 }
