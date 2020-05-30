8a60302fe50d604442cf32da8747257881565ace
Ishaan Harry Kavoori <ishaanharry@live.com>
2020-05-30 00:20:43 -0700

    started creating splash-main page

--- src/pages/splash-main/index.jsx
+++ src/pages/splash-main/index.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import Navbar from '../../general_components/navBar';
 
-
+import './style.css';
 
 class splashMain extends React.Component {
     render() {
         return(
             <div>
                 <Navbar isListPage='false' />
-                
-           
+                <div className="image_container" href={this.props.image}>
+                  <img src={this.props.image} alt="" />
+                </div>
+                <div className="info_container">
+                  <h1 className="title">Welcome to {this.props.title}!</h1>
+                  <h2 className="info">We will start in {this.props.startTime} minutes</h2>
+                  <h2 className="info">Food is arriving at {this.props.foodArrival} minutes! Remember to check in!</h2>
+                  <h2 className="info">Check-In Code: {this.props.code}</h2>
+                </div>
             </div>
         );
     }
 }
+
+splashMain.propTypes = {
+  title: PropTypes.string.isRequired,
+  startTime: PropTypes.string.isRequired,
+  foodArrival: PropTypes.string.isRequired,
+  code: PropTypes.string.isRequired,
+};
 
 export default splashMain;
