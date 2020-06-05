3d2ef216968b452294bd4f331f447722825e88ba
michl1001 <56284221+michl1001@users.noreply.github.com>
2020-06-04 23:41:49 -0400

    Event form (#10)
    * added README file to general_components
    
    * added blank index.jsx files
    
    * Basic Navbar setup
    
    * Added Assets folder and styling the navbar
    
    * created Rainbow class in NavBar
    
    * implemented basic navBar
    
    * NavBar displays on default page
    
    * fixed bugs so app will compile
    
    * Fixed navBar styling
    
    * manifest fix
    
    * Finished basic navBar
    
    * Styled Navbar correctly
    
    * Made the navBar responsive
    
    * Added basic Splash-Form page
    
    * Basic setup for Specific Event Page
    
    * started creating splash-main page
    
    * Implemented the createEvent form design
    
    * trying to create the spash-main page
    
    * changed splash-main from class to function - still trying to pass props
    
    Co-authored-by: Ishaan Harry Kavoori <ishaanharry@live.com>
    Co-authored-by: leestanley <stanleylee91776@gmail.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,15 +4,24 @@ import './style.css';
 import {ReactComponent as Diamond} from '../assets/diamonds.svg';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import splashForm from './splash-form';
+import splashMain from './splash-main';
 
 class Page extends React.Component {
   render() {
     return (
-      <Router>
+        <Router>
           <Switch>
+
             <Route exact path='/' component={splashForm} />
             {/*<Route path='/list' component={splash-listing} />*/}
-            {/*<Route path='/event' render={(props) => <Dashboard {...props} event={} />} /> {/* this is the page, yo can pass in props here*/}
+            <Route path='/event' component={splashMain}
+                    render={(routeProps) => <splashMain {...routeProps}
+                      image="https://static-template.acmucsd.com/static/media/logo.34f30e9c.png"
+                      title="skribl memes"
+                      startTime="4:20"
+                      foodArrival="4:20"
+                      code="m3m3s"/>}
+            /> {/* this is the page, yo can pass in props here*/}
           </Switch>
           <div className="diamonds">
             <Diamond />
